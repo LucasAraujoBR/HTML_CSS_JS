17ec5b0a076153133fd7bc7215c90dc9d9ed87bb
Lucas Araújo <lucasaraujoti73@gmail.com>
2021-07-28 13:08:12 -0300

    Build: Complete project

---
+++ EastBank page/script.js
@@ -0,0 +1,10 @@
+const toggle_btn = document.querySelector('.dropdown-toggle');
+const navigation = document.querySelector('header .navigation')
+
+toggle_btn.addEventListener('click', () =>{
+    if(navigation.classList.contains('active')){
+        navigation.classList.remove('active')
+    } else {
+        navigation.classList.add('active')
+    }
+    })
