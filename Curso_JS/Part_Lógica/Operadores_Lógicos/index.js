4fb21d97a1fc02e052a6efe82a9647e0d1b2bb39
Lucas Araujo <lucaraj1412@gmail.com>
2021-08-31 16:16:47 -0300

    Update: adicionado novas aulas de js

---
+++ Curso_JS/Part_Lógica/Operadores_Lógicos/index.js
@@ -0,0 +1,22 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || false || false;
+
+// const usuario = 'Luiz'; // form usuário digitou
+// const senha = '123456'; // form usuário digitou
+
+// //                      True                  False
+// const vaiLogar = usuario === 'Luiz' && senha === '12345';
+
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true && false -> vai retornar "o valor verdadeiro"
+*/
+
+
+console.log(!!true);
